2db1fa2e534a522c3611c930cc2a7533d33f6455
Najeeb Ullah Shah <najeebus777@gmail.com>
2017-10-12 19:32:23 +0500

    chatbot.js change url

--- chatbot_data/static/javascript/chatbot.js
+++ chatbot_data/static/javascript/chatbot.js
@@ -8,7 +8,7 @@ $('document').ready(function () {
             var content = {
                 'question': questionText
             };
-            var url = 'http://localhost:8000/ask-question/';
+            var url = 'https://cryptic-hamlet-56422.herokuapp.com/ask-question/';
             var csrftoken = jQuery("[name=csrfmiddlewaretoken]").val();
             function csrfSafeMethod(method) {
                 return (/^(GET|HEAD|OPTIONS|TRACE)$/.test(method));
